013d96b348173958e8bc86d90bdebdec7601424a
dmtr636 <dmtr636@gmail.com>
2022-10-28 23:16:10 +0300

    Add content

--- src/components/Location/Location.tsx
+++ src/components/Location/Location.tsx
@@ -6,11 +6,10 @@ import React from "react";
 export const Location = () => {
 	return (
 		<InfoSection title={"Местоположение"} contentType={"custom"} id={"location"}>
-			<StaticImage
-				src={"../../images/location/map.jpg"}
-				alt={""}
-				placeholder={"blurred"}
-			/>
+			<iframe
+				src="https://yandex.ru/map-widget/v1/?um=constructor%3Aaae5a552d11f4bd220bc9153d60ede48266e970278afab250d277c499209cc62&amp;source=constructor"
+				width="1200" height="435" frameBorder="0">
+			</iframe>
 		</InfoSection>
 	)
 }
