516a1dc9ad8e819fcc8a4df1b53ee4ff2bf35a1b
dmtr636 <dmtr636@gmail.com>
2022-10-30 20:21:34 +0300

    Mobile version

--- src/components/Location/Location.tsx
+++ src/components/Location/Location.tsx
@@ -8,7 +8,7 @@ export const Location = () => {
 			<div className={style.location}>
 				<iframe
 					src="https://yandex.ru/map-widget/v1/?um=constructor%3Aaae5a552d11f4bd220bc9153d60ede48266e970278afab250d277c499209cc62&amp;source=constructor"
-					width="1200" height="435" frameBorder="0">
+					width="1200" height="435" frameBorder="0" loading="lazy">
 				</iframe>
 			</div>
 		</InfoSection>
